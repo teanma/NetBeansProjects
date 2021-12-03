5a199fb641d8fdd08ab7e3faabb86de26d048915
teanma <mardaleteodorandrei@gmail.com>
2021-12-03 09:32:15 +0100

    Update
    Upload

---
+++ Ej4_Segundo_Trimestre/src/main/java/com/mycompany/ej4_segundo_trimestre/EventosDelRatón.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ej4_segundo_trimestre;
+
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author TeodorAndreiMardale
+ */
+public class EventosDelRatón {
+    
+    public static void main(String[] args) {
+        JFrame v = new JFrame();
+        v.getContentPane().setLayout(new FlowLayout());
+        JButton limpiar = new JButton("limpiar");
+        
+    }
+}
